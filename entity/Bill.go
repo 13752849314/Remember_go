2af1b580504fb3b83609d7b52126cc60931ec5d0
happygh0st <ouao_happygh0st@163.com>
2024-05-08 21:27:37 +0800

    20240508

--- entity/Bill.go
+++ entity/Bill.go
@@ -6,13 +6,13 @@ import (
 
 type Bill struct {
 	ID           uint               `gorm:"primary_key;column:id" json:"id"`
-	CreatedAt    common.MyDateTime  `gorm:"column:created_at" json:"created_at"`
-	UpdatedAt    common.MyDateTime  `gorm:"column:updated_at" json:"updated_at"`
-	DeletedAt    *common.MyDateTime `sql:"index" gorm:"column:deleted_at" json:"deleted_at"`
+	CreatedAt    common.MyDateTime  `gorm:"column:created_at;type:datetime" json:"created_at"`
+	UpdatedAt    common.MyDateTime  `gorm:"column:updated_at;type:datetime" json:"updated_at"`
+	DeletedAt    *common.MyDateTime `sql:"index" gorm:"column:deleted_at;type:datetime" json:"deleted_at"`
 	Username     string             `gorm:"column:username" json:"username"`
 	ConsumeType  int                `gorm:"column:consumeType" json:"consumeType"`
 	ConsumeMoney float64            `gorm:"column:consumeMoney" json:"consumeMoney"`
-	ConsumeTime  common.MyDateTime  `gorm:"column:consumeTime" json:"consumeTime"`
+	ConsumeTime  common.MyDateTime  `gorm:"column:consumeTime;type:datetime" json:"consumeTime"`
 	Remark       string             `gorm:"column:remark" json:"remark"`
 }
 
--- entity/User.go
+++ entity/User.go
@@ -8,14 +8,14 @@ import (
 
 type User struct {
 	ID        uint               `gorm:"primary_key;column:id" json:"id"`
-	CreatedAt common.MyDateTime  `gorm:"column:created_at" json:"created_at"`
-	UpdatedAt common.MyDateTime  `gorm:"column:updated_at" json:"updated_at"`
-	DeletedAt *common.MyDateTime `sql:"index" gorm:"column:deleted_at" json:"deleted_at"`
+	CreatedAt common.MyDateTime  `gorm:"column:created_at;type:datetime" json:"created_at"`
+	UpdatedAt common.MyDateTime  `gorm:"column:updated_at;type:datetime" json:"updated_at"`
+	DeletedAt *common.MyDateTime `sql:"index" gorm:"column:deleted_at;type:datetime" json:"deleted_at"`
 	Username  string             `gorm:"column:username;unique" form:"username" json:"username"`
 	Password  string             `gorm:"column:password" form:"password" json:"password"`
 	Phone     string             `gorm:"column:phone" form:"phone" json:"phone"`
 	Email     string             `gorm:"column:email" form:"email" json:"email"`
-	Birthday  common.MyDate      `gorm:"column:birthday" form:"birthday" json:"birthday"`
+	Birthday  common.MyDate      `gorm:"column:birthday;type:datetime" form:"birthday" json:"birthday"`
 	OpenId    string             `gorm:"column:openId" form:"openId" json:"openId"`
 	Roles     string             `gorm:"column:roles" form:"roles" json:"roles"`
 }
